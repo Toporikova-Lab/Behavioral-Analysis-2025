aa9ea9c953e1d96679fe160428b932fa83f243a1
Ayoub-Winder <tayoub-winder@wlu.edu>
2025-06-05 14:44:20 -0400

    add working raster plot

--- Raster plot/process.R
+++ Raster plot/process.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(cowplot)
 
 process <- function(filename, header=FALSE, sep='\t') {
   df <- read.csv(filename, header, sep)
@@ -17,7 +18,43 @@ process <- function(filename, header=FALSE, sep='\t') {
 }
 
 lineplot <- function(data, spiderid) {
-  ggplot(data=data, mapping=aes(x=datetime,y=(s1 > 0)*max(s1))) +
-    geom_ribbon(aes(ymin=0, ymax=light*max(s1)), fill="#ffff66") +
-    geom_bar(stat="identity")
+  ggplot(data=data, mapping=aes(x=datetime)) +
+    geom_ribbon(aes(ymin=0, ymax=light), fill="#ffff66") +
+    geom_ribbon(aes(ymin=0, ymax=(get(spiderid)>0)*1), stat="identity", fill="#101010")
 }
+
+rasterplot <- function(data, spiderid, start_dt = NULL) {
+  if (is.null(start_dt)) {
+    start_dt <- data$datetime[1]
+  }
+  
+  end_dt <- tail(data$datetime, 1)
+  
+  dts <- seq(start_dt, end_dt, by="day")
+  
+  print(dts)
+  
+  plots <- list()
+  
+  for (dt in dts) {
+    data_day <- data[dt < data$datetime & data$datetime < dt + 24*60*60,]
+    
+    if (dt == tail(dts, 1)) {
+      plot <- lineplot(data_day, spiderid) + xlim(dt, dt + 24*60*60)
+    }
+    else {
+      plot <- lineplot(data_day, spiderid) +
+        xlab('') +
+        ylab('') +
+        theme(axis.text.x=element_blank(),
+              axis.ticks.x=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks.y=element_blank())
+    }
+    
+    plots <- append(plots, list(plot))
+  }
+  
+  plot_grid(plotlist=plots, nrow=length(plots), ncol=1)
+}
+
